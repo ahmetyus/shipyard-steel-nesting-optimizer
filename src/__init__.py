a8fae85a3b4cb035f4c5045e4bdc9b99bbc2d517
AHMET YUSUF GENÇER <151620474+ahmetyus@users.noreply.github.com>
2026-03-19 23:16:17 +0200

    Initialize module with imports for components

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .models import SteelPart, SteelPlate
+from .genetic_algorithm import GeneticNestingOptimizer
+from .nesting_engine import NestingEngine
+from .visualization import NestingVisualizer
